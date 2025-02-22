2e2b22a4ba95961333cece540165ec06f1c71169
ockyo <ockyo.0605@gmail.com>
2025-02-22 18:57:38 +0700

    update: delete, create method

--- src/services/GiftService/GiftDataService.js
+++ src/services/GiftService/GiftDataService.js
@@ -3,5 +3,13 @@ class GiftDataService {
     getAllGifts() {
         return apiService.get("/Gift/GetListGift");
     }
+    deleteGift(giftId) {
+        return apiService.delete(`/Gift/DeleteGift/?id=${giftId}`);
+    }
+    createGift(newGift) {
+        return apiService.post("/Gift/CreateGift", newGift);
+    }
 }
-export default new GiftDataService();
+
+const giftDataService = new GiftDataService()
+export default giftDataService;
